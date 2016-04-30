90e557458948550c14a735170243b7a62b803c12
ShivaNambiar <skngn@hotmail.com>
2016-04-29 23:40:11 -0400

    first working file with testng, maven and fast/slow tests

--- src/test/java/gmailSignInTest.java
+++ src/test/java/gmailSignInTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,14 +11,42 @@ import org.testng.annotations.Test;
  * Created by Adirosshi on 4/29/2016.
  */
 public class gmailSignInTest {
-    @Test
+    WebDriver driver;
+
+
+    @Test(groups={"fast"})
     public void signIn()
     {
-        WebDriver driver= new FirefoxDriver();
+        String browserName = System.getenv("browser");
+
+        if(browserName !=null && browserName.equalsIgnoreCase("chrome"))
+        {
+            System.setProperty("webdriver.chrome.driver", "C:\\shiva\\Testing\\Testing\\Selenium\\ChromeDriver/chromeDriver.exe");
+            driver= new ChromeDriver();
+        }
+        else
+        driver = new FirefoxDriver();
   //      WebDriverWait wait = new WebDriverWait(driver,20);
 //        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.partialLinkText("Inbox")));
         driver.get("http://www.gmail.com");
         System.out.println(driver.getTitle());
         Assert.assertEquals(driver.getTitle(),"Gmail","checking title of homepage");
+        System.out.println("fast1");
+    }
+    @Test(groups = {"fast"})
+    public void fastTest()
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://wwww.yahoomail.com");
+        System.out.println("fast2");
+    }
+
+
+    @Test(groups = {"slow"})
+    public void slowTest()
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.bloomberg.com");
+        System.out.println("sloooooooow");
     }
 }
