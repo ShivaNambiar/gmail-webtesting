2031ed22ad30bf25bd8b25aec793cccd25856ee9
ShivaNambiar <skngn@hotmail.com>
2016-04-29 20:03:53 -0400

    Initial SETUP

---
+++ src/test/java/gmailSignInTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Adirosshi on 4/29/2016.
+ */
+public class gmailSignInTest {
+    @Test
+    public void signIn()
+    {
+        WebDriver driver= new FirefoxDriver();
+  //      WebDriverWait wait = new WebDriverWait(driver,20);
+//        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.partialLinkText("Inbox")));
+        driver.get("http://www.gmail.com");
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(),"Gmail","checking title of homepage");
+    }
+}
